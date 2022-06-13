93305d8270db3f21d6d67eeba7051c8309b835e1
Igor <igor.borowski@capgemini.com>
2022-06-13 16:14:33 +0200

    replace IProduct to IData for fetch data

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,20 +1,17 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { IProduct } from "src/app/product/model/products.model";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { IProduct } from 'src/app/product/model/products.model';
+import { IData } from '../model/api.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ProductService {
-    url: string = 'http://localhost:8000/data'
-    constructor(private http: HttpClient) {
+  url: string = 'http://localhost:8000/data';
+  constructor(private http: HttpClient) {}
 
-    }
-
-    public getProducts(): Observable<IProduct[] | any> {
-        return this.http.get<IProduct[]>(this.url)
-    }
-
+  public getProducts(): Observable<IData> {
+    return this.http.get<IData>(this.url)
+  }
 }
