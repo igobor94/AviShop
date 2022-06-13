195aa9dc11746515dab6478c1876d1635ce083e0
Igor <igor.borowski@capgemini.com>
2022-06-13 16:13:41 +0200

    inject service into list & change selectors name

--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/core/services/product.service';
+import { IProduct } from '../../model/products.model';
 
 @Component({
-  selector: 'app-product-list',
+  selector: 'product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: IProduct[] = []
+
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe(response => this.products = [...this.products, ...response.products])
   }
 
 }
