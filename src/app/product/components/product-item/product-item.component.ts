7369d065f8eefde0ef5152fe573585e186101690
Igor <igor.borowski@capgemini.com>
2022-06-13 16:11:35 +0200

    generate product item & implement product in

---
+++ src/app/product/components/product-item/product-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from '../../model/products.model';
+
+@Component({
+  selector: 'product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+export class ProductItemComponent implements OnInit {
+  @Input() product: IProduct = {id: 0, title: ''}
+  
+
+  ngOnInit(): void {
+    console.log(this.product)
+  }
+
+
+}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
 
 @NgModule({
   declarations: [
     ProductComponent,
     ProductListComponent,
     ProductDetailsComponent,
+    ProductItemComponent,
   ],
   imports: [CommonModule],
   exports: [ProductComponent, ProductListComponent, ProductDetailsComponent],
