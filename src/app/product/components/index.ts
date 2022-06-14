d112e2e4b0bdf4c714beeddd8215c8a5c76ac3cf
Igor <igor.borowski@capgemini.com>
2022-06-14 16:44:03 +0200

    arragement product

---
+++ src/app/product/components/index.ts
@@ -0,0 +1,4 @@
+export * from './product/product.component'
+export * from './product-details/product-details.component'
+export * from './product-item/product-item.component'
+export * from './product-list/product-list.component'
