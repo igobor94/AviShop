9708116336628d3c7565794bf458335168856fcc
Igor <igor.borowski@capgemini.com>
2022-06-27 11:38:06 +0200

    first steps of auth routes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent, RegisterComponent } from './';
 
 
 @NgModule({
