9589bb4b3d60b3af40c51285063ad8dd9012ce6f
Igor <igor.borowski@capgemini.com>
2022-10-14 17:07:35 +0200

    matchpasswords validator

--- src/app/shared/validators/match-password.validator.ts
+++ src/app/shared/validators/match-password.validator.ts
@@ -1,19 +1,5 @@
 import { FormGroup } from "@angular/forms";
-export function matchPasswords(password: string, confirmPassword: string) {
-  console.log('here')
-    return (formGroup: FormGroup) => {
-      const passwordControl = formGroup.controls[password]
-      const confirmPasswordControl = formGroup.controls[confirmPassword]
-      
-      console.log(passwordControl !== confirmPasswordControl)
 
-      if (confirmPasswordControl.errors && !confirmPasswordControl.errors['matchPasswords']) {
-        return
-      }
-      if (passwordControl !== confirmPasswordControl ) {
-        return confirmPasswordControl.setErrors({ matchPasswords: true })
-      } else {
-        return confirmPasswordControl.setErrors(null)
-      }
-    };
-  }
+export function matchPasswords(form: FormGroup) {
+  return form.controls['password'].value === form.controls['confirmPassword'].value ? null : {'mismatch': true};
+}
