67b094110f9db9b1c2f59b695a5c96ddc696ff5f
Igor <igor.borowski@capgemini.com>
2022-10-21 14:20:48 +0200

    generate & export password strength component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ErrorComponent } from './error/error.component';
 import { NotificationComponent } from './notification/notification.component';
+import { PasswordStrengthComponent } from './password-strength/password-strength.component';
 
 @NgModule({
   declarations: [
     ErrorComponent,
     NotificationComponent,
+    PasswordStrengthComponent,
   ],
   imports: [CommonModule],
-  exports: [NotificationComponent, ErrorComponent],
+  exports: [NotificationComponent, ErrorComponent, PasswordStrengthComponent],
 })
 export class SharedModule {}
