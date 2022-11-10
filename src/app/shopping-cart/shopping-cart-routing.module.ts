3ac8336c5d9acf1551e5e489dd9bd11cd99de978
Igor <igor.borowski@capgemini.com>
2022-11-10 10:09:32 +0100

    create nav button & routing

--- src/app/shopping-cart/shopping-cart-routing.module.ts
+++ src/app/shopping-cart/shopping-cart-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ShoppingCartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
