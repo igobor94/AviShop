8f9f7302d0cafdc7ccdaabdcc25580087bcd25e0
Igor <igor.borowski@capgemini.com>
2022-11-10 09:39:56 +0100

    create first components

--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShoppingCartRoutingModule } from './shopping-cart-routing.module';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ShoppingCartListComponent } from './components/shopping-cart-list/shopping-cart-list.component';
+import { ShoppingCartItemComponent } from './components/shopping-cart-item/shopping-cart-item.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ShoppingCartComponent,
+    ShoppingCartListComponent,
+    ShoppingCartItemComponent
+  ],
   imports: [
     CommonModule,
     ShoppingCartRoutingModule
