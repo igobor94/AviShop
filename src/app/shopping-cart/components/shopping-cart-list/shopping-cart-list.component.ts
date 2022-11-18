16e9019c5d8a97a7d920c29cc3baacd3220f9181
Igor <igor.borowski@capgemini.com>
2022-11-18 11:13:23 +0100

    change types in shopping cart components

--- src/app/shopping-cart/components/shopping-cart-list/shopping-cart-list.component.ts
+++ src/app/shopping-cart/components/shopping-cart-list/shopping-cart-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IShoppingCart } from 'src/app/product/model/shopping-cart.model';
 
 @Component({
   selector: '[shopping-cart-list]',
@@ -7,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ShoppingCartListComponent implements OnInit {
 
-  @Input() listOfProducts: any = []
+  @Input() listOfProducts: IShoppingCart[] = []
 
   constructor() { }
 
